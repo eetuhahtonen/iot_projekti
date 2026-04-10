62438ebcca50770b4eacb411a1449d34ffe92468
Eetu Hahtonen <eetu.hahtonen@icloud.com>
2026-04-10 11:48:59 +0300

    tehtävä 4 osa 1

---
+++ osuus_2_tehtävä_4/post_usage.py
@@ -0,0 +1,41 @@
+from time import sleep
+import os
+
+from dotenv import load_dotenv
+import psutil
+import requests
+
+load_dotenv("../.env")
+
+WRITE = os.getenv("WRITE_API_KEY")
+URL = f'https://api.thingspeak.com/update?api_key={WRITE}&'
+FIELDS = {
+    "cpu":"field4",
+    "ram":"field3"
+}
+
+
+def main():
+    counter = 0
+    
+    while True:
+        post = " POST" if not counter % 8 else ""
+        stats = {
+        "cpu":psutil.cpu_percent(),
+        "ram":psutil.virtual_memory().percent
+        }
+        
+        if post:
+            for key, field in FIELDS.items():
+                requests.post(f"{URL}{field}={stats[key]}")
+        
+        print(f"cpu: {stats['cpu']:>5.2f}% ram: {stats['ram']:>5.2f}%{post}")
+        counter += 1
+        sleep(2)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\ninterrupted")
